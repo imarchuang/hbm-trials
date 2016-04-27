83e25ff90064b46b2fed9a5fba4b4d60a21c8997
imarchuang <huanghe622@gmail.com>
2016-04-27 01:41:15 -0700

    hello world project with annotating the Hibernate domain model

---
+++ hbm-helloworld-annotation/src/main/java/com/imarchuang/hbm/helloworld/Message.java
@@ -0,0 +1,61 @@
+package com.imarchuang.hbm.helloworld;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MESSAGES")
+public class Message {
+	
+	@Id
+    @GeneratedValue
+    @Column(name = "MESSAGE_ID")
+	private Long id;
+	@Column(name = "MESSAGE_TEXT")
+	private String text;
+	@ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "NEXT_MESSAGE_ID")
+	private Message nextMessage;
+
+	public Message() {
+	}
+
+	public Message(String text) {
+		this.text = text;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Message getNextMessage() {
+		return nextMessage;
+	}
+
+	public void setNextMessage(Message nextMessage) {
+		this.nextMessage = nextMessage;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public String toString() {
+		return "Message{" + "id=" + id + ", text=" + text + ", nextMessage=" + nextMessage + '}';
+	}
+}
